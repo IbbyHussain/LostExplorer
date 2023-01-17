3f75740aff584e779ac3a6a2f9ed95cb05b37fc1
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2023-01-17 17:16:49 +0000

    Turret
    turret fires projectile at player location

--- Assets/Player/AI/Turret/TurretAI.cs
+++ Assets/Player/AI/Turret/TurretAI.cs
@@ -9,6 +9,22 @@ public class TurretAI : MonoBehaviour
     float Distance;
     public float TurretSight; // distance until turret will fire at player
 
+    public GameObject Projectile;
+
+    public Transform TurretBarrelTransform;
+
+    // Fire Rate
+    public float FireRate;
+    float NextFire;
+
+    void TurretFire() 
+    {
+        // Instantiate projectile
+        GameObject ProjectileRef = Instantiate(Projectile, TurretBarrelTransform.position, TurretBarrelTransform.rotation);
+        ProjectileRef.GetComponent<Rigidbody>().AddForce(TurretBarrelTransform.up * 1500f * -1f);
+        Destroy(ProjectileRef, 5.0f);
+    }
+
     void Start()
     {
         // Get player transform
@@ -22,7 +38,15 @@ public class TurretAI : MonoBehaviour
         if(Distance <= TurretSight) 
         {
             transform.LookAt(PlayerTransform);
+
+            // Adjustment as mesh was rotated wrong in blender
             transform.Rotate(-90, 0, 0);
+
+            if(Time.time >= NextFire) 
+            {
+                NextFire = Time.time + 1f / FireRate;
+                TurretFire();
+            }
         }
     }
 }
